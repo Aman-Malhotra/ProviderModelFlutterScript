c7ef86a279b8ee6454bd18b48f510fd4a52852b6
Aman-malhotra <official.aman18@gmail.com>
2020-04-27 02:31:47 +0530

    Added working code. Able to add widget and provider files

--- make_flutter_files_script.sh
+++ make_flutter_files_script.sh
@@ -1,62 +1,132 @@
 #!/bin/bash
 
-# # Go inside the flutter project to add new files
-# cd shopLocal/lib
+getStatefulWidgetCode() {
+    fileName=$1
+    echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'package:shopLocal/ProviderModels/'${fileName}$'ProviderModel/'${fileName}$'_pm.dart\';\r\n\nclass '${fileName^}$' extends StatefulWidget {\r\n@override\r\n_'${fileName^}$'State createState() => _'${fileName^}$'State();\r\n}\r\n\r\nclass _'${fileName^}$'State extends State<'${fileName^}$'> {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
+}
 
-# array=("logo")
+getProviderModelCode() {
+    fileName=$1
+    echo $'import \'package:flutter/material.dart\';\r\n\nclass '${fileName^}'Pm extends ChangeNotifier {\r\n'${fileName^}'Pm();\r\n}'
+}
 
-# # Add dart widget files for modules
-# if [ -d "$DIRECTORY" ]; then
-#   # Control will enter here if $DIRECTORY exists.
-# fi
-# mkdir "Widgets"
-# cd "Widgets"
-# for i in "${array[@]}"; do
-#     i=${i//-/}
-#     i=${i//_/}
-#     mkdir "${i}Widget"
-#     cd "${i}Widget"
-#     touch "${i}.dart"
-#     code=$'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'package:shopLocal/ProviderModels/'${i}$'ProviderModel/'${i}$'_pm.dart\';\r\n\nclass '${i^}$' extends StatefulWidget {\r\n@override\r\n_'${i^}$'State createState() => _'${i^}$'State();\r\n}\r\n\r\nclass _'${i^}$'State extends State<'${i^}$'> {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${i^}'Pm '${i}'Pm = Provider.of<'${i^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
-#     echo -e $code >>"${i}.dart"
-#     cd ..
-# done
-# cd ..
+# Folder for adding widget files
+addWidgetsFolder() {
+    folderName="Widgets"
+    if [ -d "${folderName}" ]; then
+        echo ${folderName}$' folder already exists.\nEntering into the folder.'
+    else
+        mkdir "${folderName}"
+    fi
+    cd "${folderName}"
+}
+# Folder for adding Provider Model files
+addProviderModelsFolder() {
+    folderName="ProviderModels"
+    if [ -d "${folderName}" ]; then
+        echo ${folderName}$' folder already exists.\nEntering into the folder.'
+    else
+        mkdir "${folderName}"
+    fi
+    cd "${folderName}"
+}
 
-# # Add dart provider model files for modules
-# mkdir "ProviderModels"
-# cd "ProviderModels"
-# for i in "${array[@]}"; do
-#     i=${i//-/}
-#     i=${i//_/}
-#     mkdir "${i}ProviderModel"
-#     cd "${i}ProviderModel"
-#     touch "${i}_pm.dart"
-#     code=$'import \'package:flutter/material.dart\';\r\n\nclass '${i^}'Pm extends ChangeNotifier {\r\n'${i^}'Pm();\r\n}'
-#     echo -e $code >>"${i}_pm.dart"
-#     cd ..
-# done
-# cd ..
+addWidgetFile() {
+    fileName=$1
+    addWidgetsFolder ${fileName}
+    mkdir "${fileName}Widget"
+    moveInsideLocation "${fileName}Widget"
+    touch "${fileName}.dart"
+    statefulWidgetCode=$(getStatefulWidgetCode ${fileName})
+    echo -e $statefulWidgetCode >>"${fileName}.dart"
+    # echo "${fileName}.dart added inside ${fileName}Widget"
+    cd ..
+    cd ..
+    echo "${fileName}.dart added"
+}
 
-# echo $'Make sure you have "provider:" already added under "dependencies:" section inside "pubspec.yaml" file.'
-#########################################################################################
+addProviderFile() {
+    fileName=$1
+    addProviderModelsFolder ${fileName}
+    mkdir "${fileName}ProviderModel"
+    moveInsideLocation "${fileName}ProviderModel"
+    touch "${fileName}_pm.dart"
+    providerModelCode=$(getProviderModelCode ${fileName})
+    echo -e $providerModelCode >>"${fileName}_pm.dart"
+    cd ..
+    cd ..
+    echo "${fileName}_pm.dart added"
+}
 
-echo $'Follow the steps to add the provider model\nBoth the Provider class and Widget class(Stateful Widget) will be automatically added to your project and linked with proper imports.'
-while true; do
-    echo "Name the WIDGET you are building (try using 'camelCasing')"
-    read -p "Name - " fileName
-    echo '-------------'
-    echo $fileName' Added' 
-    echo '-------------'
-    read -p $'Do you want to add another WIDGET? y/N \n' yn
-    case $yn in
-        [Yy]* ) 
+moveInsideLocation() {
+    locaiton=$1
+    cd ${locaiton}
+}
+
+askForProjetcLocation() {
+    while true; do
+        read -p $'Are you inside your Flutter project\'s root location ? y/N\n' yN
+        echo ''
+        case $yN in
+        [Yy]*)
             # Nothing here as of now if user enters yes
-        ;;
-        [Nn]* ) 
-            echo "No"
-            exit;;
-        * ) 
-            echo "Please answer yes or no.(y/N)";;
-    esac
+            echo $'Moving inside lib\n'
+            cd lib
+            echo ""
+            break;;
+        [Nn]*)
+            # ask user for project root location
+            read -p $'Please paste your project root location here\n' projectLocation
+            moveInsideLocation ${projectLocation}
+            cd lib
+            echo ""
+            break;;
+        *)
+            echo "Please answer yes or no.(y/N)"
+            ;;
+        esac
+    done
+}
+
+# --------------------------------------------------------------- #
+############ START OF SCRIPT ################
+echo $'Follow the steps to add the provider model files\nBoth the Provider class and Widget class(Stateful Widget) will be automatically added to your project and linked with proper imports.'
+############## ASK USER FOR PROJECT LOCATION ##########
+askForProjetcLocation
+######################################################
+
+############# AFTER ENTERING INTO /lib FOLDER OF FLUTTER PROJECT ############
+echo $'Name the WIDGET you are building (try using "camelCasing") \nEg - signIn, signUp, dahboard, mainPage, firstPage\n'
+while true; do
+    read -p "Name - " fileName
+    fileName=${fileName//-/}
+    fileName=${fileName//_/}
+    nameLength=${#fileName}
+    minLength=0
+    if
+        [ $nameLength -gt $minLength ]
+    then
+        echo '-------------'
+        addWidgetFile ${fileName}
+        addProviderFile ${fileName}
+        echo ''
+        echo $fileName$' Widget and ProviderModel added and linked.'
+        echo '-------------'
+        read -p $'Do you want to add another WIDGET? y/N \n' yn
+        case $yn in
+        [Yy]*)
+            # Nothing here as of now if user enters yes
+            continue;;
+        [Nn]*)
+            echo $'Make sure you have "provider:" already added under "dependencies:" section inside "pubspec.yaml" file.'
+            echo "Exiting from program..."
+            exit
+            ;;
+        *)
+            echo "Please answer yes or no.(y/N)"
+            ;;
+        esac
+    else
+        echo "Please enter a valid file name again."
+    fi
 done
