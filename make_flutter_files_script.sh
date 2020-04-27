3c7762c63459cc3a7b106a10ef869ff51b46d0c7
Aman-malhotra <official.aman18@gmail.com>
2020-04-27 14:22:02 +0530

    Added option for  stateful or stateless widget

--- make_flutter_files_script.sh
+++ make_flutter_files_script.sh
@@ -7,7 +7,55 @@ getStatefulWidgetCode() {
 
 getProviderModelCode() {
     fileName=$1
-    echo $'import \'package:flutter/material.dart\';\r\n\nclass '${fileName^}'Pm extends ChangeNotifier {\r\n'${fileName^}'Pm();\r\n}'
+    echo $'import \'package:flutter/material.dart\';\r\n\nclass '${fileName^}$'Pm extends ChangeNotifier {\r\nString sampleText = "Sample Text";\r\n'${fileName^}$'Pm(){\r\n//This is the constructor\r\n}\r\n}'
+}
+
+getStatelessWidgetCode() {
+    fileName=$1
+    echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'package:shopLocal/ProviderModels/'${fileName}$'ProviderModel/'${fileName}$'_pm.dart\';\r\n\nclass '${fileName^}$' extends StatelessWidget {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
+}
+
+getWidgetCode() {
+    fileName=$1
+    option=$2
+    statelessCode=1
+    statefulCode=2
+    # If option == 1
+    # getStatelessWidgetCode()
+    # If option == 2
+    # getStatelessWidgetCode()
+    widgetCode=""
+    if [ ${option} -eq ${statelessCode} ]; then
+        # echo "Inside 1"
+        widgetCode=$(getStatelessWidgetCode ${fileName})
+    elif [ ${option} -eq ${statefulCode} ]; then
+        # echo "Inside 2"
+        widgetCode=$(getStatefulWidgetCode ${fileName})
+    else
+        widgetCode=$(getStatelessWidgetCode ${fileName})
+    fi
+    echo ${widgetCode}
+}
+
+askForStatefulOrStatelessWidget() {
+    while true; do
+        read -p $'Do you want to make it a stateful or stateless Widget? 1/2\n1. Stateless Widget (Default)\n2. Stateful Widget\n' option
+        echo ''
+        case $option in
+        [1]*)
+            echo 1
+            break
+            ;;
+        [2]*)
+            echo 2
+            break
+            ;;
+        *)
+            echo 1
+            break
+            ;;
+        esac
+    done
 }
 
 # Folder for adding widget files
@@ -37,9 +85,9 @@ addWidgetFile() {
     mkdir "${fileName}Widget"
     moveInsideLocation "${fileName}Widget"
     touch "${fileName}.dart"
-    statefulWidgetCode=$(getStatefulWidgetCode ${fileName})
-    echo -e $statefulWidgetCode >>"${fileName}.dart"
-    # echo "${fileName}.dart added inside ${fileName}Widget"
+    option=$(askForStatefulOrStatelessWidget)
+    code=$(getWidgetCode ${fileName} ${option})
+    echo -e $code >>"${fileName}.dart"
     cd ..
     cd ..
     echo "${fileName}.dart added"
@@ -73,14 +121,16 @@ askForProjetcLocation() {
             echo $'Moving inside lib\n'
             cd lib
             echo ""
-            break;;
+            break
+            ;;
         [Nn]*)
             # ask user for project root location
-            read -p $'Please paste your project root location here\n' projectLocation
+            read -p $'Please paste your Flutter project root location here\n' projectLocation
             moveInsideLocation ${projectLocation}
             cd lib
             echo ""
-            break;;
+            break
+            ;;
         *)
             echo "Please answer yes or no.(y/N)"
             ;;
@@ -106,17 +156,18 @@ while true; do
     if
         [ $nameLength -gt $minLength ]
     then
-        echo '-------------'
+        echo '----------------'
         addWidgetFile ${fileName}
         addProviderFile ${fileName}
         echo ''
         echo $fileName$' Widget and ProviderModel added and linked.'
-        echo '-------------'
+        echo '----------------'
         read -p $'Do you want to add another WIDGET? y/N \n' yn
         case $yn in
         [Yy]*)
             # Nothing here as of now if user enters yes
-            continue;;
+            continue
+            ;;
         [Nn]*)
             echo $'Make sure you have "provider:" already added under "dependencies:" section inside "pubspec.yaml" file.'
             echo "Exiting from program..."
