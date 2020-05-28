fdb73a10046777ffad7e8db6fa8369f97e80f085
Aman-malhotra <official.aman18@gmail.com>
2020-05-28 23:16:56 +0530

    File structure changed

--- providerFilesScript.sh
+++ providerFilesScript.sh
@@ -4,17 +4,18 @@ projectName="<project_name>"
 
 getStatefulWidgetCode() {
     fileName=$1
-    echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'package:'${projectName}'/ProviderModels/'${fileName}$'ProviderModel/'${fileName}$'_pm.dart\';\r\n// Copy the line below and add as a child where you want to use '${fileName^}$' Widget\r\n// ChangeNotifierProvider(create:(context)=>'${fileName^}$'Pm(),child: '${fileName^}$'())\r\n\nclass '${fileName^}$' extends StatefulWidget {\r\n@override\r\n_'${fileName^}$'State createState() => _'${fileName^}$'State();\r\n}\r\n\r\nclass _'${fileName^}$'State extends State<'${fileName^}$'> {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName,}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
+    # echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'package:'${projectName}'/ProviderModels/'${fileName}$'ProviderModel/'${fileName}$'_pm.dart\';\r\n// Copy the line below and add as a child where you want to use '${fileName^}$' Widget\r\n// ChangeNotifierProvider(create:(context)=>'${fileName^}$'Pm(),child: '${fileName^}$'())\r\n\nclass '${fileName^}$' extends StatefulWidget {\r\n@override\r\n_'${fileName^}$'State createState() => _'${fileName^}$'State();\r\n}\r\n\r\nclass _'${fileName^}$'State extends State<'${fileName^}$'> {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName,}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
+    echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'./'${fileName}$'_pm.dart\';\r\n// Copy the line below and add as a child where you want to use '${fileName^}$' Widget\r\n// ChangeNotifierProvider(create:(context)=>'${fileName^}$'Pm(),child: '${fileName^}$'())\r\n\nclass '${fileName^}$' extends StatefulWidget {\r\n@override\r\n_'${fileName^}$'State createState() => _'${fileName^}$'State();\r\n}\r\n\r\nclass _'${fileName^}$'State extends State<'${fileName^}$'> {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName,}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
 }
 
 getProviderModelCode() {
     fileName=$1
-    echo $'import \'package:flutter/material.dart\';\r\n\nclass '${fileName^}$'Pm extends ChangeNotifier {\r\nString sampleText = "Sample Text";\r\n'${fileName^}$'Pm(){\r\n//This is the constructor\r\n}\r\n}'
+    echo $'import \'package:flutter/material.dart\';\r\n\nclass '${fileName^}$'Pm extends ChangeNotifier {\r\n'${fileName^}$'Pm(){\r\n//This is the constructor\r\n}\r\n}'
 }
 
 getStatelessWidgetCode() {
     fileName=$1
-    echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'package:'${projectName}'/ProviderModels/'${fileName}$'ProviderModel/'${fileName}$'_pm.dart\';\r\n// Copy the line below and add as a child where you want to use '${fileName^}$' Widget\r\n// ChangeNotifierProvider(create:(context)=>'${fileName^}$'Pm(),child: '${fileName^}$'())\r\n\nclass '${fileName^}$' extends StatelessWidget {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName,}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
+    echo $'import \'package:flutter/material.dart\';\r\nimport \'package:provider/provider.dart\';\r\nimport \'./'${fileName}$'_pm.dart\';\r\n// Copy the line below and add as a child where you want to use '${fileName^}$' Widget\r\n// ChangeNotifierProvider(create:(context)=>'${fileName^}$'Pm(),child: '${fileName^}$'())\r\n\nclass '${fileName^}$' extends StatelessWidget {\r\n @override\r\n  Widget build(BuildContext context) {\r\n'${fileName^}'Pm '${fileName,}'Pm = Provider.of<'${fileName^}'Pm>(context);\r\nreturn Container();\r\n }\r\n}'
 }
 
 getWidgetCode() {
@@ -83,23 +84,17 @@ addProviderModelsFolder() {
 
 addWidgetFile() {
     fileName=$1
-    addWidgetsFolder ${fileName}
-    mkdir "${fileName}Widget"
-    moveInsideLocation "${fileName}Widget"
+    mkdir "${fileName}"
+    moveInsideLocation "${fileName}"
     touch "${fileName}.dart"
     option=$(askForStatefulOrStatelessWidget)
     code=$(getWidgetCode ${fileName} ${option})
     echo -e $code >>"${fileName}.dart"
-    cd ..
-    cd ..
     echo "${fileName}.dart added"
 }
 
 addProviderFile() {
     fileName=$1
-    addProviderModelsFolder ${fileName}
-    mkdir "${fileName}ProviderModel"
-    moveInsideLocation "${fileName}ProviderModel"
     touch "${fileName}_pm.dart"
     providerModelCode=$(getProviderModelCode ${fileName})
     echo -e $providerModelCode >>"${fileName}_pm.dart"
@@ -145,6 +140,14 @@ askForProjetcLocation() {
     done
 }
 
+moveIntoWidgets() {
+    if [ ! -d "Widgets" ]; then
+        echo $'No lib folder found.\nCreating lib folder'
+        mkdir Widgets
+    fi
+    moveInsideLocation "Widgets"
+}
+
 # --------------------------------------------------------------- #
 ############ START OF SCRIPT ################
 echo $'Follow the steps to add the provider model files\nBoth the Provider class and Widget class(Stateful Widget) will be automatically added to your project and linked with proper imports.'
@@ -164,6 +167,9 @@ while true; do
         [ $nameLength -gt $minLength ]
     then
         echo '----------------'
+
+        moveIntoWidgets
+
         addWidgetFile ${fileName}
         addProviderFile ${fileName}
         echo ''
